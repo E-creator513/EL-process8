771c9495e82257218533584cf1678fa1ba6cec89
E-creator513 <emmanuelzvavamwe428@gmail.com>
2026-02-02 12:45:57 +0300

    Update Airflow DAGs and Docker setup

--- Desktop/DAG-process/push.sh
+++ Desktop/DAG-process/push.sh
@@ -8,6 +8,6 @@ git add .
 
 git commit -m "Update Airflow DAGs and Docker setup"
 
-git remote add origin https://github.com/E-creator513/EL-process.git
+git remote add origin https://github.com/E-creator513/EL-process8.git
 
 git push origin $(git rev-parse --abbrev-ref HEAD)
